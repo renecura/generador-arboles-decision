fe604ab44cacd96254c674b955ddea014c027eaa
Rodrigo Rene Cura <rodrigo.renecura@gmail.com>
2021-04-23 17:47:29 -0300

    Resolucion del árbol

--- modules/generador-arboles.js
+++ modules/generador-arboles.js
@@ -6,7 +6,8 @@ const VAL = 'valor';
 function generar_punto_decision(options) {
   const result = {
     type: DP,
-    value: undefined,
+    min_value: undefined,
+    max_value: undefined,
     children: options
   };
 
@@ -35,8 +36,8 @@ function generar_evento(options) {
   return result;
 }
 
-function generar_opcion(options) {
-  return (Math.random() > 0.5) ? generar_evento(options) : generar_punto_decision(options);
+function generar_opcion(options, p) {
+  return (Math.random() < p) ? generar_evento(options) : generar_punto_decision(options);
 }
 
 
@@ -63,22 +64,40 @@ function generar(n_options = 12, max_options = 4) {
   while (options.length > 1) {
     options.push(
       generar_opcion(
-        options.splice(0, randRange(2, Math.min(options.length, max_options)))
+        options.splice(0, randRange(2, Math.min(options.length, max_options))),
+        options.length * 2 / n_options
       )
     );
   }
 
-  console.log(options);
+  const rs = resolver(options[0]); 
 
-  return options[0];
+  console.log(rs);
+
+  return rs;
 }
 
 function resolver(tree) {
 
-  // Hacer un recorrido en profundidad del arbol y calcular los valores.
+  // Cacula el value de todos los hijos.
+  if(tree.children)
+    tree.children = tree.children.map(resolver);
+  
+  switch (tree.type) {
+    case EV: 
+      tree.value = tree.children.reduce((acc, x) => acc + x.probability * x.value,0);
+      break;
+    case DP: 
+      tree.max_value = Math.max(...tree.children.map(x => x.value? x.value : x.max_value)); 
+      tree.min_value = Math.min(...tree.children.map(x => x.value? x.value : x.min_value));
+      break;
+  }
 
+  //tree.value = Math.round(tree.value * 100) / 100.0;
+
+  // Retorna el valor esperado del árbol.
   return tree;
 }
 
 
-export { generar }
+export { generar, resolver }
